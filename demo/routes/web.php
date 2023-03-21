f4452f8d28587a36b9d74b3f417a3fdb527f25a3
vladrzvk <vladislav.razyvika@gmail.com>
2023-03-21 15:03:52 +0100

    update

--- demo/routes/web.php
+++ demo/routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-  $visited = DB::select('select * from places where visited = ?', [1]); 
-  $togo = DB::select('select * from places where visited = ?', [0]);
+  // $visited = DB::select('select * from places where visited = ?', [1]); 
+  // $togo = DB::select('select * from places where visited = ?', [0]);
 
-  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
+  // return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
+  return view('welcome');
 });
