56f52e8d46f95ecd2f2334239898c9ead3013f81
vladrzvk <vladislav.razyvika@gmail.com>
2023-03-27 11:05:06 +0200

    update init db init

--- mysql/init_db.sql
+++ mysql/init_db.sql
@@ -1,12 +1,126 @@
--- DROP TABLE IF EXISTS places;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db:3306
+-- Généré le : lun. 27 mars 2023 à 09:03
+-- Version du serveur : 5.7.41
+-- Version de PHP : 8.1.15
 
--- CREATE TABLE places (
---   id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
---   name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
---   visited tinyint(1) NOT NULL DEFAULT "0",
---   PRIMARY KEY (id)
--- ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- INSERT INTO places (name, visited) VALUES ("Berlin",0),("Budapest",0),("Cincinnati",1),("Denver",0),("Helsinki",0),("Lisbon",0),("Moscow",1),("Nairobi",0),("Oslo",1),("Rio",0),("Tokyo",0);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pickmanager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `folders`
+--
+
+CREATE TABLE `folders` (
+  `id` int(10) NOT NULL,
+  `id_owner` int(10) NOT NULL,
+  `name` varchar(66) NOT NULL,
+  `description` text NOT NULL,
+  `creation` date NOT NULL,
+  `access` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `id_folder` int(11) NOT NULL,
+  `image` blob NOT NULL,
+  `posted` date NOT NULL,
+  `lieu` text NOT NULL,
+  `description` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `first_name` varchar(535) NOT NULL,
+  `last_name` varchar(535) NOT NULL,
+  `email` varchar(535) NOT NULL,
+  `password` varchar(535) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `folders`
+--
+ALTER TABLE `folders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `owner` (`id_owner`);
+
+--
+-- Index pour la table `images`
+--
+ALTER TABLE `images`
+  ADD KEY `folder` (`id_folder`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `folders`
+--
+ALTER TABLE `folders`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `folders`
+--
+ALTER TABLE `folders`
+  ADD CONSTRAINT `owner` FOREIGN KEY (`id_owner`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `folder` FOREIGN KEY (`id_folder`) REFERENCES `folders` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
